ee290e234dad76cb7a4b3f7abe54e94f0894b4ca
projectdx <com114@gmail.com>
2021-08-08 11:22:58 +0900

    aaaa

--- logic_linkkfyommi.py
+++ logic_linkkfyommi.py
@@ -375,7 +375,7 @@ class LogicLinkkfYommi(object):
 
             # tmp = tree.xpath('//*[@id="relatedpost"]/ul/li')
             tmp = tree.xpath(
-                '/html/body/article/div[1]/article/a')
+                '//*[@id="syno-nsc-ext-gen3"]/article/div[1]/article')
 
             logger.info(tmp)
             if tmp is not None:
